c4216d155729659b1791103d3f3567fca2ffdd87
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2022-10-11 02:44:59 +0300

    [feat] author view which is detail and list

--- src/auser/views.py
+++ src/auser/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Author
+
+class AuthorListView(ListView):
+
+    model = Author
+    template_name = "auser/authors_list.html"
+    context_object_name = "authors"
+    extra_context = {"title": "Authors list"}
+        
+class AuthorDetailView(DetailView):
+
+    model = Author
+    template_name = 'auser/author_detail.html'
+    extra_context = {"title": "Author detail"}
