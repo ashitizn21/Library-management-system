78c75ae356b4d2fa1ace1451d769faa55970386d
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2022-10-12 13:03:06 +0300

    [feat] role functions is registered

--- src/auser/apps.py
+++ src/auser/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class AuserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auser'
+
+    def ready(self):
+        ''' Create roles if not exist after migration '''
+        from auser.signals import create_role
+        post_migrate.connect(create_role, self)
