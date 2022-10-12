ecf2e203331f6ad2ea78cf3c129b2f1d990fcb93
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2022-10-12 13:05:45 +0300

    [feat] usercreation form

---
+++ src/auser/forms.py
@@ -0,0 +1,15 @@
+
+from django import forms
+from auser.models import User
+
+class UserCreateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "phone_number",
+            "location",
+        )
