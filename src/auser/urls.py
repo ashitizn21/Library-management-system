2724248459dd4e8c8f6bae70b8f6fc9cc94609b9
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2022-10-13 03:29:02 +0300

    [feat] user and roles url mapper is done

--- src/auser/urls.py
+++ src/auser/urls.py
@@ -60,4 +60,39 @@ urlpatterns = [
 
     re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors_list'),
     re_path(r'^author/(?P<pk>\d+)/$', views.AuthorDetailView.as_view(), name='author_detail'),
+    
+    re_path(
+        r'^user/',
+        include(
+            [
+                re_path(
+                    r'add/$',
+                    views.AddUserView.as_view(),
+                    name="add_user"
+                ),
+                re_path(
+                    r"^lists/$",
+                    views.ListUsersView.as_view(),
+                    name="list_user"
+                 ),
+            ]
+        )
+    ),
+    re_path(
+        r'^roles/',
+        include(
+            [
+                re_path(
+                    r'^list/$',
+                    views.ListRolesView.as_view(),
+                    name="role_list"
+                ),
+                re_path(
+                    r'^update/(?P<pk>\d+)/$',
+                    views.UpdateRoleView.as_view(),
+                    name="update_role"
+                ),
+            ],
+        )
+    ),
 ]
