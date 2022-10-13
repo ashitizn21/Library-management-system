cbd98ac84c57c69305e2cc144f504d47838b1919
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2022-10-13 03:28:13 +0300

    [feat] update role form is done

--- src/auser/forms.py
+++ src/auser/forms.py
@@ -1,5 +1,9 @@
 
+from dataclasses import field
+from pyexpat import model
 from django import forms
+from django.contrib.auth.models import Group, Permission
+
 from auser.models import User
 
 class UserCreateForm(forms.ModelForm):
@@ -13,3 +17,13 @@ class UserCreateForm(forms.ModelForm):
             "phone_number",
             "location",
         )
+
+class UpdateRoleForm(forms.ModelForm):
+    ''' Form used to update permission of a group user '''
+    permissions = forms.ModelMultipleChoiceField(
+        queryset=Permission.objects.select_related("content_type"), required=False
+    )
+
+    class Meta:
+        model = Group
+        fields = ("permissions", )
