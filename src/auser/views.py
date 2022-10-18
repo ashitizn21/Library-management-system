d773c5c933ac05e68429dd9489bdd8ed77253b85
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2022-10-18 14:35:12 +0300

    [format] correct direcotry of author files

--- src/auser/views.py
+++ src/auser/views.py
@@ -14,14 +14,14 @@ from .forms import UserCreateForm, UpdateRoleForm, UpdateUserForm
 class AuthorListView(ListView):
 
     model = Author
-    template_name = "auser/authors_list.html"
+    template_name = "auser/author/list.html"
     context_object_name = "authors"
     extra_context = {"title": "Authors list"}
         
 class AuthorDetailView(DetailView):
 
     model = Author
-    template_name = 'auser/author_detail.html'
+    template_name = 'auser/author/detail.html'
     extra_context = {"title": "Author detail"}
 
 class AddUserView(PermissionRequiredMixin, SuccessMessageMixin, CreateView):
