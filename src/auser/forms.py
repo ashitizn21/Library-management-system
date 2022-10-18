39e00640379f422f1e9d00f09d80525b00a585d0
Ashenafi Zenebe <ashenafizenebe19@gmail.com>
2022-10-18 23:28:04 +0300

    [feat] add author form is done

--- src/auser/forms.py
+++ src/auser/forms.py
@@ -1,8 +1,9 @@
 import datetime
+from pyexpat import model
 from django import forms
 from django.contrib.auth.models import Group, Permission
 from django.core.exceptions import ValidationError
-from auser.models import User
+from auser.models import User, Author
 
 class UserCreateForm(forms.ModelForm):
     class Meta:
@@ -44,3 +45,16 @@ class UpdateUserForm(forms.ModelForm):
         "po_box",
         "profile_picture",
     )
+
+class DateInput(forms.DateInput):
+    input_type  = 'date'
+
+class AddAuthorForm(forms.ModelForm):
+
+    class Meta:
+        model = Author
+        fields = ['username', "first_name", "last_name", "email", "phone_number", "sex", "date_of_birth", "date_of_death", "profile_picture"]
+        widgets = {
+            "date_of_birth": DateInput(),
+            "date_of_death": DateInput(),
+        }
